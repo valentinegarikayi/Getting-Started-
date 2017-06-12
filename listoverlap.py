4aadd89bd8e132ef8e58d671890fcb3a61114b32
dmnfarrell <farrell.damien@gmail.com>
2017-06-12 11:54:25 +0100

    lists overlap example

---
+++ listoverlap.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#this is the solution to
+#http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+
+import random
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+#print the overlap of both lists
+
+
+#generate random lists and do the same thing
+
+
