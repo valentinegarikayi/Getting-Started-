9690bc4be995c856798d7b6db773cd490ba79e7d
Francis Garikayi <fran.garikayi@gmail.com>
2017-06-14 11:12:37 +0100

    my solution to df problem

--- listoverlap.py
+++ listoverlap.py
@@ -3,14 +3,13 @@
 #this is the solution to
 #http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
 
-import random
+import random# random module imported here...is it?
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 #print the overlap of both lists
-
-
+for i in a and b:#this reads list a and b
+    if i in a and b:#this is a condition to check both numbers-why not use equal?
+        print(i)
 #generate random lists and do the same thing
-
-
