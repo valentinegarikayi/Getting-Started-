f9c146ad9b40f8c4222f02f80fc34e7469c2d10f
valentinegarikayi <fran.garikayi@gmail.com>
2017-06-22 11:50:47 +0100

    all additions

---
+++ duplicates.py
@@ -0,0 +1,7 @@
+
+a=[input('Eneter an element: ')for i in range(5)]
+b=[]
+for i in a:
+    if i==i:
+        b.append(i)
+print(b)
---
+++ list_set_comprehension.py
@@ -0,0 +1,4 @@
+
+    a=[input('Enter a name: ')for i in range(5)] #create a list of 10 elements
+    b=set(a)#set function looks for unique elements only
+    print(b)
