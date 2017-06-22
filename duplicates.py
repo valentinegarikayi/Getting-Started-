585c743b98a1d247e80c42276002d5ea39223643
dmnfarrell <farrell.damien@gmail.com>
2017-06-22 16:52:11 +0100

    duplicates fix

--- duplicates.py
+++ duplicates.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python
 
-a=[input('Eneter an element: ')for i in range(5)]
+#a=[input('Eneter an element: ')for i in range(5)]
+a=[4,6,7,8,3,4,6,1]
+
 b=[]
 for i in a:
-    if i==i:
+    if not i in b:
         b.append(i)
-print(b)
+
+print (b)
+#sorted version of b
+print (sorted(b))
+
