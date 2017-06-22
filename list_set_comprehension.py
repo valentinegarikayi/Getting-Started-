f6f349f3bd04e1b3dc7c010ce277e8fcbc6ac859
valentinegarikayi <fran.garikayi@gmail.com>
2017-06-22 14:50:40 +0100

    changes to indentation

--- list_set_comprehension.py
+++ list_set_comprehension.py
@@ -1,4 +1,4 @@
-
-    a=[input('Enter a name: ')for i in range(5)] #create a list of 10 elements
-    b=set(a)#set function looks for unique elements only
-    print(b)
+#!/usr/bin/env python
+a=[input('Enter a name: ')for i in range(5)] #create a list of 10 elements
+b=set(a)#set function looks for unique elements only
+print(b)
