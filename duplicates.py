a72f86294936b3db76f1b5553b935fc8decd42cf
valentinegarikayi <fran.garikayi@gmail.com>
2017-06-22 17:23:50 +0100

    questions to df

--- duplicates.py
+++ duplicates.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
-a=['John', 'Peter', 'Dick', 'Dick', 'Francis', 'Git', 'John']
-b=[]
-for i in a:
-    b.append(i)
-print(b)
-=======
 
 #a=[input('Eneter an element: ')for i in range(5)]
 a=[4,6,7,8,3,4,6,1]
 
 b=[]
 for i in a:
-    if not i in b:
+    if not i in b:# I need an explanation of what this does...my question is because it seems b is nil and we are testing
         b.append(i)
 
 print (b)
 #sorted version of b
 print (sorted(b))
-
->>>>>>> 585c743b98a1d247e80c42276002d5ea39223643
