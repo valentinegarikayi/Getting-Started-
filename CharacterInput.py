6202abe8a051b4db17a349e2cf874b940b9d3a3d
dmnfarrell <farrell.damien@gmail.com>
2017-06-27 13:07:43 +0100

    fixes for char input

--- CharacterInput.py
+++ CharacterInput.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+
 import datetime
 name = input('What is your name: ')
 age = int (input ('What is your age: '))
-d= datetime.datetime.today()# why does it repeat datetime? Is a function called by a variable?
+d = datetime.datetime.today() # datetime is the module, with a function of the same name being called
 century = d.year + (100-age)
-message = ('Your name is {}'+ name, 'you are{}',age,{}'years old and you will turn 100 years in{}',century.format(name,age,century))
-numb= int(input('Please give me any number: '))
-print (message*numb)
+message = ('Your name is {}. You are {} years old and you will turn 100 years in {}.'.format(name,age,century))
+numb = int(input('Please give me any number: '))
+print (message)
