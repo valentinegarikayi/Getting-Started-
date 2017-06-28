7ff3340adbf69b8f4faa936197fe8b35010822cd
valentinegarikayi <fran.garikayi@gmail.com>
2017-06-28 09:05:06 +0100

    Odd or even solved

--- OddorEven.py
+++ OddorEven.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+number = int(input("Please give me a number: "))
+if number % 2 != 0:
+    print ('The number you gave me is an odd number')
+elif number % 4 ==0 and number % 2 ==0:
+        print ('The number you gave me is even and a multiple of four git!')
+else:
+    print ('The number you gave me is an even number')
