70b3ac8849c694875a1a61c687ea8dac9145ea9c
valentinegarikayi <fran.garikayi@gmail.com>
2017-06-29 16:01:58 +0100

    Strinlists bug

---
+++ String_Lists.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#this is the solution to:
+#http://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
+# here I attempt reversing the string using double colon and it works
+user=input('Please give me a string:')
+back =user[::-1]
+print(back)
+# here I attempt reversing the string using reverse() and it doesnt work
+user=input('Please give me a string:')
+user.reverse
+print(user)
