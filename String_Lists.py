240f088ffede46eab683b5418d2f52486535604c
valentinegarikayi <fran.garikayi@gmail.com>
2017-06-29 16:06:35 +0100

    small change

--- String_Lists.py
+++ String_Lists.py
@@ -7,5 +7,5 @@ back =user[::-1]
 print(back)
 # here I attempt reversing the string using reverse() and it doesnt work
 user=input('Please give me a string:')
-user.reverse
+user.reverse()
 print(user)
