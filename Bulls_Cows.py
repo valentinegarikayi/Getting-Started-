3c91ced29f6e8c9a5f928ff2fc22eed16e4042f8
Francis Garikayi <fran.garikayi@gmail.com>
2017-08-30 21:52:24 +0100

    indendations

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -4,10 +4,12 @@
 #http://www.practicepython.org/exercise/2014/07/05/18-cows-and-bulls.html#
 
 import random
+print ('Welcome to the Cows and Bulls Game')#Welcome message
 a= [random.randint(1,4)for i in range(4)]#generates the random number
 numb=int(input('Please guess what number I am: '))#accepts the random number-this is defined as a variable
 #DF please explain why it couldnt traverse is as a list
 b=[int(i) for i in str(numb)]#converts the number into a string
+c=[]#this stores all the guesses
 for x in a:
             if x in a and b:
                 print('1 Cow')
