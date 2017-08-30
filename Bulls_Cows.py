07b9e24cc35041b79c38844a1017a8aa20f86a8f
Francis Garikayi <fran.garikayi@gmail.com>
2017-08-30 21:37:27 +0100

    Bulls updated

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -5,8 +5,12 @@
 
 import random
 a= [random.randint(1,4)for i in range(4)]#generates the random number
-numb=[int(input('Please guess what number I am'))]#accepts the random number
-c=[int(i) for i in str(numb)]#converts the number into a string
-
-
-b=[]
+numb=int(input('Please guess what number I am: '))#accepts the random number-this is defined as a variable
+#DF please explain why it couldnt traverse is as a list
+b=[int(i) for i in str(numb)]#converts the number into a string
+    for x in a:
+        if x in a and b:
+            print('1 Cow')
+        else:
+            print('1 Bull')
+            
