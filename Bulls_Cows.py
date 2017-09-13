0397c431c099b72bfedfbc5dce969b7b4c459d3a
Francis Garikayi <fran.garikayi@gmail.com>
2017-09-13 23:49:39 +0200

    New solution to bulls

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -5,21 +5,18 @@
 
 import random
 print ('Welcome to the Cows and Bulls Game')#Welcome message
-a = [random.randint(1,4) for i in range(4)] #generates the random number
-#accepts the random number-this is defined as a variable
-numb = input('Please guess a 4 digit number: ')
-#converts the number into a list
-b = [int(i) for i in numb]
-c = [] #this stores all the guesses
-count=0
-if a != b:
-	for i in zip(a,b):
-		if i[0] == i[1]:
-			c.append('Cow')
-		else:
-			c.append('Bull')
-	print(c)
-	count = count + 1
-	print('You have guessed',count, 'times')
+while a != b:
+	count=0
+	a = [random.randint(1,4) for i in range(4)]
+    numb = input('Please guess a 4 digit number: ')
+    b = [int(i) for i in numb]
+    c = [] #this stores all the guesses
+    for i in zip(a,b):
+        if i[0] == i[1]:
+            c.append('Cow')
+        else:
+            c.append('Bull')
+            print(c)
+			count=count+1
 else:
-	print('You guessed correctly and the game is over')
+	print('You guessed', count, 'times and the game is over')
